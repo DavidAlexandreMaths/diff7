1c7099d88df1dfc1a1cacd3838f29dfe8fb562a1
David Alexandre <david1599@hotmail.fr>
2026-05-26 21:03:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -77,8 +77,10 @@ function animate() {
       currentTurns = targetTurns;
     }
 
-    roue1.rotation.x = currentTurns * Math.PI * 2;       // 👈 axe rouge = X
-    roue2.rotation.x = -roue1.rotation.x * 3;
+   // À mettre dans animate() à la place des rotation.x
+const axeX = new THREE.Vector3(1, 0, 0); // axe X mondial
+roue1.rotateOnWorldAxis(axeX, (targetTurns - currentTurns) * Math.PI * 2 * speed);
+roue2.rotateOnWorldAxis(axeX, -(targetTurns - currentTurns) * Math.PI * 2 * speed * 3);
 
     counter1.innerText = "Première roue : " + currentTurns.toFixed(2);
     counter2.innerText = "Dernière roue : " + (-currentTurns * 3).toFixed(2);
