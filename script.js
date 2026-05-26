0bf375c080fb850680f71577e8fd55f6015274c8
David Alexandre <david1599@hotmail.fr>
2026-05-26 19:16:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,8 +53,12 @@ let roue1;
 let roue2;
 
 loader.load('animation_diff7.glb', (gltf) => {
-
   scene.add(gltf.scene);
+
+  gltf.scene.traverse(child => {
+    console.log(child.name, "→", child.type);
+  });
+});
 
   const box = new THREE.Box3().setFromObject(gltf.scene);
   const size = box.getSize(new THREE.Vector3());
