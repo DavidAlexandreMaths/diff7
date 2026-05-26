f1344424dec2e6ee8ea2d7bf249cff56a0066812
David Alexandre <david1599@hotmail.fr>
2026-05-26 17:56:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -102,12 +102,12 @@ function animate() {
     parseFloat(slider.value);
 
     // roue principale
-    roue1.rotation.y =
+    roue1.rotation.x =
       turns * Math.PI * 2;
 
     // ratio engrenages
-    roue2.rotation.y =
-      -roue1.rotation.y * 3;
+    roue2.rotation.x =
+      -roue1.rotation.x * 3;
 
     // compteurs
     counter1.innerText =
